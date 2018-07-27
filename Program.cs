a4eec85a74c9e2beb9083767889065c3f7efa85b
abadied <abadi.eden@gmail.com>
2018-07-27 15:17:37 +0300

    added write obs file

--- Program.cs
+++ Program.cs
@@ -19,18 +19,25 @@ namespace POMDP
             MazeDomain maze = new MazeDomain(path + "/Domains/Maze3.txt");
 
             // POMDP PROB AUTOMATA //
-            List<MazeAction> actions = new List<MazeAction> {
+            List<Action> actions = new List<Action> {
             new MazeAction("TurnLeft"),
             new MazeAction("TurnRight"),
             new MazeAction("Forward")
             };
 
-            List<MazeObservation> observations = new List<MazeObservation>();
+            List<Observation> observations = new List<Observation>();
             for (int i = 0; i < 16; i++)
                 observations.Add(new MazeObservation(i));
 
             ConvertionFunction cf = new ConvertionFunction(actions, observations);
+            RandomPolicy p0 = new RandomPolicy(maze);
 
+            string firstPath = "";
+            int numberOfIterations = 100;
+            int numberOfSteps = 100;
+            int currBit = 0;
+            // TODO: add for loop that creates obs file for each bit ( add new paths as well).
+            maze.WriteObservationsFile(firstPath, cf, p0, numberOfIterations, numberOfSteps, currBit);
             // POMDP PROB AUTOMATA //
 
 
@@ -44,8 +51,8 @@ namespace POMDP
             //MostLikelyStatePolicy p1 = new MostLikelyStatePolicy(v);
             //VotingPolicy p2 = new VotingPolicy(v);
             //QMDPPolicy p3 = new QMDPPolicy(v, maze);
-
-            double dADR1 = maze.ComputeAverageDiscountedReward(p1, 100, 100);
+            
+            //double dADR1 = maze.ComputeAverageDiscountedReward(p1, 100, 100);
             //double dADR2 = maze.ComputeAverageDiscountedReward(p2, 100, 100);
             //double dADR3 = maze.ComputeAverageDiscountedReward(p3, 100, 100);
             //double dADR4 = maze.ComputeAverageDiscountedReward(pbvi, 100, 100);
